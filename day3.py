8d27306982ca6be054f9e8b909406c58c42d4022
Chris <chris.nguyen@hcss.com>
2023-12-16 16:03:58 -0600

    continue day3

--- day3.py
+++ day3.py
@@ -1,20 +1,31 @@
-# adjacent if
-# same col or +/- 1 col, +/- 1 row
-# same row, +/- 1 col
-# 
-
 f = open("input\\day3.txt")
-# symbols = []
-# schematic = []
+nums = []
+schematic = []
 
-# for i,line in enumerate(f):
-#     schematic.append(line)
+def checkCharacter(c, nums, num):
+    if (not c.isdigit()) and c != ".":
+        nums.append(num)
 
-# for i,row in enumerate(schematic):
-#     for j,c in row:
-#         if (c.isdigit()):
-#             print(c)
+for line in f:
+    schematic.append(line.replace('\n', ""))
 
+numLocs = []
+for row,line in enumerate(schematic):
+    num = []
+    for col,c in enumerate(line):
+        if c.isdigit():
+            num.append(c)
+        elif len(num) > 0:
+            numLocs.append(("".join([str(item) for item in num]), row, col - 1))
+            num = []
 
-# print(schematic)
+for num,row,col in numLocs:
+    if row == 0 and col - (len(num) - 1) == 0:
+        for c in schematic[row + 1][0:len(num) + 1]:
+            checkCharacter(c, nums, num)
+        checkCharacter(schematic[row][len(num) + 1], nums, num)
+
+print(schematic)
+print(numLocs)
+print(nums)
 f.close()
