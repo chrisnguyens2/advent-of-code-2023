2f8fde40881c429acdd93a3fadd80235299931b8
Chris <chris.nguyen@hcss.com>
2023-12-16 06:49:58 -0600

    refactor day2 part 1

--- day2.py
+++ day2.py
@@ -1,38 +1,24 @@
 f = open("input\day2.txt")
 
-games = []
-
-for line in f:
-    setsString = line.split(":")
-    sets = setsString[1].split(";")
-    setArray = []
-    games.append(setArray)
-
-    for set in sets:
-        colorCounts = set.split(",")
-        countDict = {}            
-        setArray.append(countDict)
-
-        for colorCount in colorCounts:
-            pairArray = colorCount.lstrip().rstrip().split(" ")
-            countDict[pairArray[1]] = int(pairArray[0])
-
-#print(games)
-
 bag = {"red": 12, "green": 13, "blue": 14}
 validGames = []
 
-for i in range(len(games)):
+for i,line in enumerate(f):
+    setsString = line.split(":")
+    sets = setsString[1].split(";")
     validGame = True
+    
+    for set in sets:
+        colorCounts = set.split(",")
 
-    for set in games[i]:
-        for color, count in set.items():
-            if set[color] <= bag[color]:
+        for colorCount in colorCounts:
+            pairArray = colorCount.lstrip().rstrip().split(" ")
+            if int(pairArray[0]) <= bag[pairArray[1]]:
                 validGame = True
             else:
                 validGame = False
                 break
-        
+
         if validGame == False:
             break
 
