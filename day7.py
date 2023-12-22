c415bb09a2247bed17e9a2c5ddff89934d4e5dbc
Chris Nguyen <chris.nguyen@hcss.com>
2023-12-22 09:39:32 -0600

    day 7 create hands

--- day7.py
+++ day7.py
@@ -1,8 +1,35 @@
+import re
+
 f = open("input\\day7.txt")
+cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
 result = 0
+hands = []
+
+fiveKinds = []
+for c in cards:
+    fiveKinds.append(c+c+c+c+c)
+
+fourKinds = []
+for c in cards:
+    for i in range(0, 4):
+        for cc in cards:
+            if cc != c:
+                fk = c+c+c+c
+                fourKinds.append(fk[:i] + cc + fk[i:])
+
+threeKinds = []
+for c in cards:
+    for i in range(0, 4):
+        for cc in cards:
+            tk = c+c+c
+            fourKinds.append(fk[:i] + cc + fk[i:])
 
 for l in f:
-    print(l)
+    hands.append((l.split(" ")[0], int(l.split(" ")[1].replace("\n","") )))
 
+for h in hands:
+    print(re.findall("QQQ",h[0]))
+
+print(hands)
 print(result)
 f.close()
